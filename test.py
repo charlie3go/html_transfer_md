00d7d662779ac232e0594f756401e7aebf6d9a56
charlie3go <aslongrushan@163.com>
2025-04-24 03:16:19 +0000

    html transfer md

---
+++ test.py
@@ -0,0 +1,7 @@
+from html_converter import html_converter
+
+
+if __name__ == '__main__':
+    html = "<table><tr><th>Header</th></tr><tr><td>Data</td></tr></table>"
+    text_md = html_converter(html, verbose=True)
+    print(text_md)
